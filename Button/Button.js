5edef913a935de37486373dcde56239aea2c46a0
alexandermylnikov <56112004+alexandermylnikov@users.noreply.github.com>
2021-05-28 17:11:11 +0200

    Update Button.js
    add rounded ripple for the Android devices

--- Button/Button.js
+++ Button/Button.js
@@ -16,7 +16,7 @@ const Button = (props) => {
     return (
         Platform.OS === 'ios'
         ? <TouchableOpacity disabled={props.disabled} style={props.style} onPress={() => _handlePress(props.onPress)}>{props.children}</TouchableOpacity> 
-        : <TouchableNativeFeedback  disabled={props.disabled} onPress={() => _handlePress(props.onPress)}><View style={props.style}>{props.children}</View></TouchableNativeFeedback>
+        : <TouchableNativeFeedback  background={JSON.stringify(props.style[1]) !== '{}' ? TouchableNativeFeedback.Ripple(null, true, props.style[0].height / 2) : TouchableNativeFeedback.Ripple()} disabled={props.disabled} onPress={() => _handlePress(props.onPress)}><View style={props.style}>{props.children}</View></TouchableNativeFeedback>
     )
 }
 
